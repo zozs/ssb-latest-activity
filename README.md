8884e51da0cea49ce6532da21d442bf1a3694704
Linus Karlsson <linus@zozs.se>
2019-12-26 14:04:21 +0100

    Try to write it using some async iterators instead.

---
+++ README.md
@@ -0,0 +1,27 @@
+# ssb-latest-activity
+
+Show latest activity of people you follow, useful to, e.g., ensure your pub still sees new stuff.
+
+## :heavy_check_mark: Things to use it for
+
+* Ensure your ssb pub/client has received the latest updates, without manually inspecting a stream of JSON objects.
+* Check in on your friends so they are all right, if they haven't been active for a while.
+
+## :x: Things to NOT use it for
+
+* Like, be creepy and stalk people. Just don't.
+
+## :running: Running and example output
+
+```
+[linus@laptop]$ node latest-activity.js
+@geQl+Lnoch9HdZbOUfiEXWDoanWXEDbGBfSy9/EWOoo=.ed25519 2019-12-26T11:31:42.134Z (test1) type: vote
+@S1h5yat/vRFUJw7ft5+U661/AblIZsdDCA6ZGOb7814=.ed25519 2019-12-26T10:49:48.936Z (zozs) type: post
+@4djb2cUbKMgjp9nIsaXT0YN1/3ksT6wJCvnoG/GMsIU=.ed25519 2019-12-26T06:31:43.431Z (yayayaya) type: vote
+@hGyy0RXjA9I91KYoHt23z0SRAoIbSMdf4pMo1t7SZfQ=.ed25519 2019-12-25T23:10:58.398Z (test2) type: contact
+@UfaJk7PVJpmkiq4+WZ+BqIsFrKlOWgNcipz6fycX1qY=.ed25519 2019-12-25T21:00:00.000Z (test3) type: (unknown)
+@PrfNx6LOH+fTtqDiB1wOyOzUOlBv1HHmVqYbHf57KMU=.ed25519 2019-12-25T17:32:26.452Z (example) type: vote
+@JKRQ59PNIj+mj5rGsfI7FN0hFUX1PEd4nCUIhSlsCFU=.ed25519 2019-12-25T08:30:10.650Z (test5) type: post
+@Dg0PUvvq+IsEkD+iQpKfMiDh9/LkqaabHgQJU05O6z0=.ed25519 2019-12-15T15:13:46.906Z (ssb.zozs.se) type: contact
+@iVPi/B73hJSpsUB8XbSnlxe3NembPXLjOfIexlYdgJA=.ed25519 2019-11-27T03:04:43.677Z (The Weekly) type: post
+```
--- latest-activity.js
+++ latest-activity.js
@@ -1,67 +1,64 @@
+const awaitable = require('pull-awaitable')
 const flatmap = require('pull-flatmap')
+const pullSort = require('pull-sort')
 const pull = require('pull-stream')
-const ssbClient = require('ssb-client')
+const ssbClientCb = require('ssb-client')
 
-ssbClient(async (err, sbot) => {
-  if (err) {
-    console.error('Failed to connect to ssb-server')
-    throw err
-  }
+const { promisify } = require('util')
+const ssbClient = promisify(ssbClientCb)
 
-  pull(
-    sbot.friends.hopStream(),
-    flatmap(e => Object.entries(e)), // expand stream to consist of [id, hops]-elements.
-    pull.filter(([id, hops]) => hops >= 0 && hops <= 1), // only myself and those i follow.
-    pull.map(([id, hops]) => sbot.createUserStream({ // get latest activity for each friend
-      id,
-      reverse: true,
-      limit: 1
-    })),
-    pull.flatten(), // stream of streams => stream
-    pull.map(extractLatestInfo), // make raw post to something easier to manage.
-    flatmap(e => [ // make stream a mix of latest-posts and about-posts.
-      pull.values([e]),
-      pull(
-        sbot.links({
-          source: e.id,
-          dest: e.id,
-          rel: 'about',
-          values: true
-        }),
-        pull.filter(data => data.value.content.name !== undefined),
-        pull.map(extractAboutInfo)
-      )]),
-    pull.flatten(), // stream of streams => stream
-    pull.reduce( // for each id, keep last self-assigned username and post info.
-      (acc, e) => ({ ...acc, ...{ [e.id]: { ...acc[e.id], ...e } } }),
-      {},
-      (err, resultObj) => {
-        // first sort results based on timestamp (reverse)
-        const results = Object.values(resultObj)
-        results.sort((a, b) => b.timestamp - a.timestamp)
-        results.map(logInfoRow2)
-        sbot.close()
-      }
+main().catch(err => console.error(`unhandled top-level exception: ${err}`))
+
+async function main () {
+  try {
+    const sbot = await ssbClient()
+
+    // Get a stream of the latest actions from everyone I follow (including myself)
+    const latestStream = pull(
+      sbot.friends.hopStream(),
+      flatmap(e => Object.entries(e)), // expand stream to consist of [id, hops]-elements.
+      pull.filter(([id, hops]) => hops >= 0 && hops <= 1), // only myself and those i follow.
+      pull.map(([id, hops]) => sbot.createUserStream({ // get latest activity for each friend
+        id,
+        reverse: true,
+        limit: 1
+      })),
+      pull.flatten(), // stream of streams => stream
+      pullSort((a, b) => b.value.timestamp - a.value.timestamp) // optional, causes buffering.
     )
+
+    for await (const post of awaitable(latestStream)) {
+      // For each post (one per user), get the latest self-assigned username for the id.
+      const id = post.value.author
+      const name = await latestSelfAssignedName(sbot, id)
+
+      const timestamp = new Date(post.value.timestamp).toISOString()
+      const type = post.value.content.type || '(unknown)'
+      console.log(`${id} ${timestamp} (${name}) type: ${type}`) 
+    }
+
+    sbot.close()
+  } catch (e) {
+    console.error('Something went wrong! Is ssb-server running?')
+    console.error(`Got error: ${e}`)
+  }
+}
+
+async function latestSelfAssignedName (sbot, id) {
+  const aboutStream = pull(
+    sbot.links({
+      source: id,
+      dest: id,
+      rel: 'about',
+      values: true
+    })
   )
-})
 
-function extractAboutInfo (post) {
-  return {
-    id: post.value.content.about,
-    name: post.value.content.name
+  let name = '(unknown)'
+  for await (const about of awaitable(aboutStream)) {
+    if (about.value.content.name !== undefined) {
+      name = about.value.content.name
+    }
   }
-}
-
-function extractLatestInfo (post) {
-  return {
-    id: post.value.author,
-    timestamp: post.value.timestamp,
-    type: post.value.content.type || '(unknown)'
-  }
-}
-
-function logInfoRow2 (friend) {
-  const timestamp = new Date(friend.timestamp).toISOString()
-  console.log(`${friend.id} ${timestamp} (${friend.name}) type: ${friend.type}`)
+  return name
 }
